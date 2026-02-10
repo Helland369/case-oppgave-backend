2258bca5cce491c7a3892cfd0942e0857842cb8e
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-02-10 15:00:45 +0100

    x

--- backend/Controller/StatsController.cs
+++ backend/Controller/StatsController.cs
@@ -1,6 +1,41 @@
-﻿namespace backend.Controller
+﻿using Backend.Model;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controller;
+
+[ApiController]
+[Route("/status")]
+
+public class StatsController : ControllerBase
 {
-    public class StatsController
+    private readonly ISqliteConnectionFactory _connection;
+
+    public StatsController(ISqliteConnectionFactory connection)
     {
+        _connection = connection;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetStats()
+    {
+        try
+        {
+            using var conn = _connection.Create();
+            using var command = conn.CreateCommand();
+
+            //query not finished
+
+            command.CommandText = @"SELECT * FROM Events WHERE ";
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            return BadRequest();
+        }
     }
 }
+
+
--- backend/Controller/StatusController.cs
+++ backend/Controller/StatusController.cs
@@ -1,6 +1,85 @@
-﻿namespace backend.Controller
+﻿using Backend.Model;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controller;
+
+[ApiController]
+[Route("/status")]
+
+public class StatusController : ControllerBase
 {
-    public class StatusController
+    private readonly ISqliteConnectionFactory _connection;
+
+    public StatusController(ISqliteConnectionFactory connection)
     {
+        _connection = connection;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetStatus([FromQuery] string? studentId = null)
+    {
+        try
+        {
+            using var conn = _connection.Create();
+            using var command = conn.CreateCommand();
+
+            if (string.IsNullOrEmpty(studentId))
+            {
+                //query for  All students
+                command.CommandText = @"
+                    SELECT 
+                        StudentId,
+                        Course,
+                        Year,
+                        Semester,
+                        Type as Status,
+                        OccurredUtc,
+                        Birthdate,
+                        City
+                    FROM Event
+                    WHERE (StudentId, Course, Year, Semester, OccurredUtc) IN (
+                        SELECT StudentId, Course, Year, Semester, MAX(OccurredUtc)
+                        FROM Event
+                        GROUP BY StudentId, Course, Year, Semester
+                    )
+                    ORDER BY StudentId, Course, Year, Semester;
+                ";
+            }
+            else
+            {
+                //query for Filtered by studentId
+                command.CommandText = @"
+                    SELECT 
+                        StudentId,
+                        Course,
+                        Year,
+                        Semester,
+                        Type as Status,
+                        OccurredUtc,
+                        Birthdate,
+                        City
+                    FROM Event
+                    WHERE StudentId = @StudentId
+                      AND (StudentId, Course, Year, Semester, OccurredUtc) IN (
+                        SELECT StudentId, Course, Year, Semester, MAX(OccurredUtc)
+                        FROM Event
+                        WHERE StudentId = @StudentId
+                        GROUP BY StudentId, Course, Year, Semester
+                    )
+                    ORDER BY Course, Year, Semester;
+                ";
+                command.Parameters.AddWithValue("@StudentId", studentId);
+            }
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            return BadRequest();
+        }
     }
 }
+
+
